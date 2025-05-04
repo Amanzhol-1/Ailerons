fc28a6539ebaa3e4e770347edcc3f1e52c152ae0
Amanzhol <amanzhol.zxc1@gmail.com>
2025-05-04 17:24:51 +0500

    update profiles

--- goGate/internal/auth/domain/profiles.go
+++ goGate/internal/auth/domain/profiles.go
@@ -1,13 +1,13 @@
 package domain
 
 type CustomerProfile struct {
-	UserID      int64  `json:"-"`
+	UserID      int64  `json:"user_id"`
 	CompanyName string `json:"company_name"`
 	Address     string `json:"address"`
 }
 
 type DriverProfile struct {
-	UserID        int64  `json:"-"`
+	UserID        int64  `json:"user_id"`
 	LicenseNumber string `json:"license_number"`
 	VehicleInfo   string `json:"vehicle_info"`
 }
